63bd9b5ce30d9a4af9fe4b917ebc7ccba99791c4
santajikadam12 <76932278+santajikadam12@users.noreply.github.com>
2021-01-31 09:39:19 -0600

    Add files via upload

---
+++ mymethod.java
@@ -0,0 +1,10 @@
+public class mymethod { static void myMethod() {
+    System.out.println("I just got executed!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+  }
+}
+    
+
