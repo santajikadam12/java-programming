4ac4595311d28e922a9d31ff26152227f92f7289
santajikadam12 <76932278+santajikadam12@users.noreply.github.com>
2021-01-31 09:38:54 -0600

    Add files via upload

---
+++ M_Method.java
@@ -0,0 +1,15 @@
+public class M_Method {
+    public static void main(String[] args) {
+        M_Method obj = new M_Method();
+        obj.Evenodd(225);
+    }
+    void Evenodd(int x){
+    if(x % 2 ==0){
+        System.out.println("Your number is Even");
+    }else{
+        System.out.println("Your number is odd");
+    }
+    }
+    }
+    
+
