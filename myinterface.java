523bbd14d99a49a5105f83bd5bbe29ae74e9b3cd
santajikadam12 <76932278+santajikadam12@users.noreply.github.com>
2021-03-08 01:03:37 -0600

    Add files via upload

---
+++ myinterface.java
@@ -0,0 +1,19 @@
+package oops;
+
+ class myinterface {
+    public void display(){
+        System.out.println("declared in interface and define in class");
+    }  
+    public static void main(String[] args) {
+        myinterface obj = new myinterface();
+        obj.display();
+        
+    }
+    
+}
+interface first{
+    void display();// declartion
+
+    }
+
+
