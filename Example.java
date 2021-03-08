af577cd8c455a10d70cc1b3bbbb7c5b9d2632580
santajikadam12 <76932278+santajikadam12@users.noreply.github.com>
2021-03-08 00:39:44 -0600

    Add files via upload

--- Example.java
+++ Example.java
@@ -1,7 +1,19 @@
+package Oops;
+import java.util.*;
+
 public class Example {
     public static void main(String[] args) {
-        long num = -1233334556L;
-        System.out.println(num);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Num=");
+        int i = sc.nextInt();// save
+        System.out.println("Enter Second num=");
+        int j = sc.nextInt();
+        int k = i + j;
+        System.out.println("Result=" + k);
+
     }
-    
+
 }
+
+
+
