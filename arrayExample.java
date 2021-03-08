3872d1e9514dfb7b100c512ce8c4d5e1e9a5c912
santajikadam12 <76932278+santajikadam12@users.noreply.github.com>
2021-03-08 01:17:09 -0600

    Add files via upload

---
+++ arrayExample.java
@@ -0,0 +1,16 @@
+public class arrayExample {
+    public static void main(String[] args) {
+        int a[]=new int[5];
+        a[0]  = 100;
+        a [1] = 101;
+        a [2] = 103;
+        a [3] = 104;
+        a [4] = 105;
+        for (int i =0; i< a.length; i++){
+            System.out.println(a[i]);
+        }
+
+
+    }
+    
+}
